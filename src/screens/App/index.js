63051c96b0c5c79f7269563206aefe311ab3e279
Tyler Geerdts <tygee713@gmail.com>
2019-12-01 18:38:55 -0500

    Added footer component

--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -8,6 +8,11 @@ const App = () => {
       <header className={classes.header}>
         <img src={'logo.png'} className={classes.logo} alt="logo" />
       </header>
+      <div className={classes.body}>
+
+      </div>
+      <footer className={classes.footer}>
+      </footer>
     </div>
   )
 }
--- src/screens/App/styles.js
+++ src/screens/App/styles.js
@@ -3,17 +3,36 @@ import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
   app: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+  },
+  body: {
+    flex: 1,
   },
   logo: {
     height: 40,
   },
-  header: {
+  footer: {
     backgroundColor: '#282c34',
-    minHeight: 50,
+    height: 50,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
     justifyContent: 'center',
+    justifySelf: 'flex-end',
+    fontSize: 14,
+    color: 'white',
+    padding: '0 25%',
+  },
+  header: {
+    backgroundColor: '#282c34',
+    height: 50,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    justifySelf: 'flex-start',
     fontSize: 14,
     color: 'white',
     padding: '0 25%',
